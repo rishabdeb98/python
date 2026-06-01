10c49f525c56c6603baa1802ca6b46440606fe78
Rishabh <educational.rd79@gmail.com>
2026-06-02 01:40:46 +0530

    used pack() and grid() next place()

---
+++ tkinter/2ndprg.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root=Tk()
+root.title("My second program")
+root.minsize(width=100,height=200)
+root.maxsize(width=300,height=1200)
+r1=Label(root,text="Good Learning")
+r1.grid(row=0,column=0)
+root.mainloop() 
