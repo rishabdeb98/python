ce29bae76c0fb363c85ab4028def191017dbe431
Rishabh <educational.rd79@gmail.com>
2026-06-07 01:11:12 +0530

    Used Entry function to add entries 6/6/26

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -1,6 +1,9 @@
 from tkinter import *
 
 root=Tk()
-b1=Button(root,text="Enter",bg="red",fg="blue",font="24",width="12",height="6")
-b1.grid(column="0",row="1")
+root.title("Entry Widget")
+root.minsize(width=200,height=400)
+root.maxsize(width=400,height=600)
+e1=Entry(root,width=20,bd=5,font=('Arial',20))
+e1.pack()
 root.mainloop()
