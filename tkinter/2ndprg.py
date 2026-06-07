37dfcbfddf40cf560d546a16b74bc70e19ec55b2
Rishabh <educational.rd79@gmail.com>
2026-06-07 12:06:34 +0530

    Added all the functions like entry,button,label..

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -4,6 +4,17 @@ root=Tk()
 root.title("Entry Widget")
 root.minsize(width=200,height=400)
 root.maxsize(width=400,height=600)
+
 l1=Label(root,text="Employee Name",fg="red",bg="yellow",font="Arial 20 bold")
 l1.place(x=0,y=10)
+
+e1=Entry(root,bd=5,font="Arial 20 bold")
+e1.place(x=0,y=50)
+
+b1=Button(root,text="submit",fg="white",bg="blue",font="Arial 20 bold")
+b1.place(x=0,y=100)
+
+l2=Label(root,text="Nothing",fg="red",bg="yellow",font="Arial 20 bold")
+l2.place(x=5,y=160)
+
 root.mainloop()  
