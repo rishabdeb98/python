aee47a7f30018226a7112acf67a2d275e1f64145
Rishabh <educational.rd79@gmail.com>
2026-06-12 01:21:20 +0530

    In input box input will be output in place of nothing

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -7,11 +7,16 @@ root.maxsize(width=400,height=600)
 
 l1=Label(root,text="Employee Name",fg="red",bg="yellow",font="Arial 20 bold")
 l1.place(x=0,y=10)
+v=StringVar()
+def edtech():
+    x=v.get()
+    print(x)
+    l2.config(text=x)
 
-e1=Entry(root,bd=5,font="Arial 20 bold")
+e1=Entry(root,bd=5,font="Arial 20 bold",textvariable=v)
 e1.place(x=0,y=50)
 
-b1=Button(root,text="submit",fg="white",bg="blue",font="Arial 20 bold")
+b1=Button(root,text="submit",fg="white",bg="blue",font="Arial 20 bold",command=edtech)
 b1.place(x=0,y=100)
 
 l2=Label(root,text="Nothing",fg="red",bg="yellow",font="Arial 20 bold")
