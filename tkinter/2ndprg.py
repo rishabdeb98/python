8549b5af9af975ff74d95ba37303a8459b066835
Rishabh <educational.rd79@gmail.com>
2026-06-12 14:25:05 +0530

    initialize v=IntVar() and and assigned values rb1=1 and rb2=0 so output if 1st opt selected than 1 and 0 respectively

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -4,11 +4,16 @@ root=Tk()
 root.title("Entry Widget")
 root.minsize(width=200,height=400)
 root.maxsize(width=400,height=600)
+v=IntVar()
+def edtech():
+    print(v.get())
 
-rb1=Radiobutton(root,text="Python",value=1)
+rb1=Radiobutton(root,text="Python",value=1,variable=v)
 rb1.pack()
 
-rb2=Radiobutton(root,text="Java",value=2)
+rb2=Radiobutton(root,text="Java",value=2,variable=v)
 rb2.pack()
 
+e1=Button(root,text="Submit",command=edtech)
+e1.pack()
 root.mainloop()  
