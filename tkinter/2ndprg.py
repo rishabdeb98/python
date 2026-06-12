121ec71b589b1ffd055b09aaedf0c779e5f914f2
Rishabh <educational.rd79@gmail.com>
2026-06-12 13:55:42 +0530

    Used Radio button

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -5,12 +5,10 @@ root.title("Entry Widget")
 root.minsize(width=200,height=400)
 root.maxsize(width=400,height=600)
 
-lb=Listbox(root,width=20)
-lb.pack()
-l1=["python","java","c++","ruby","javascript"]
-for i in l1:
-    lb.insert(END,i)
-b1=Button(root,text="remove",bg="red",command=lambda:lb.delete(ANCHOR))
-b1.pack()
+rb1=Radiobutton(root,text="Python",value=1)
+rb1.pack()
+
+rb2=Radiobutton(root,text="Java",value=2)
+rb2.pack()
 
 root.mainloop()  
