4ace34cdade680e8eeffc39c3236ae2577d981a9
Rishabh <educational.rd79@gmail.com>
2026-06-12 12:55:59 +0530

    In Listbox used list and looped through the list

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -5,12 +5,9 @@ root.title("Entry Widget")
 root.minsize(width=200,height=400)
 root.maxsize(width=400,height=600)
 
-f1=Frame()
-f1.pack()
-f2=Frame()
-f2.pack(side=BOTTOM)
-l1=Label(f1,text="Username")
-l1.pack()
-l2=Label(f2,text="Bottom")
-l2.pack()
+lb=Listbox(root,width=20)
+lb.pack()
+l1=["python","java","c++","ruby","javascript"]
+for i in l1:
+    lb.insert(END,i)
 root.mainloop()  
