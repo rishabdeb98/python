a54ceaa5508cbab599e9ec9dd6eed29a3e8cba68
Rishabh <educational.rd79@gmail.com>
2026-07-01 11:51:50 +0530

    Radiobutton used

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -1,16 +1,20 @@
 from tkinter import *
 victus=Tk()
+victus.geometry("500x500")
+victus.title("Radio Button program")
 
-victus.geometry("400x400")
-f1=Frame()
-f1.pack()
+v=IntVar()
+rb=Radiobutton(victus,text="Python",value=1,variable=v)
+rb.pack(pady=20)
 
-f2=Frame()
-f2.pack(side=BOTTOM)
+rb2=Radiobutton(victus,text="Java",value=2,variable=v)
+rb2.pack(pady=20)
 
-l1=Label(f1,text="Great Learning")
-l1.pack()
+def edtech():
+    value=v.get()
+    print(value)
 
-l2=Label(f2,text="Bottom")
-l2.pack()
+b1=Button(victus,text="submit",command=edtech)
+b1.pack(pady=20)
+
 victus.mainloop()
