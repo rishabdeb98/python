8e52fd7162350a7de23cd1b19c41668a8257d3f8
Rishabh <educational.rd79@gmail.com>
2026-07-01 12:49:04 +0530

    messagebox used to display warning,info,etc

--- tkinter/2ndprg.py
+++ tkinter/2ndprg.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 victus=Tk()
 victus.geometry("500x500")
 victus.title("Radio Button program")
@@ -11,8 +12,13 @@ rb2=Radiobutton(victus,text="Java",value=2,variable=v)
 rb2.pack(pady=20)
 
 def edtech():
-    value=v.get()
-    print(value)
+    value = v.get()
+    if value == 0:
+        messagebox.showwarning("Caution", "Please select an option")
+    else:
+        selected_option = "Python" if value == 1 else "Java"
+        messagebox.showinfo("Successful", f"You selected: {selected_option}")
+        print(selected_option)
 
 b1=Button(victus,text="submit",command=edtech)
 b1.pack(pady=20)
